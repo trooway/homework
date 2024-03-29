709894eeaaeda0dd9fee5cbda0de5f2620e2e404
trooway <160148493+trooway@users.noreply.github.com>
2024-03-29 16:53:17 +0300

    Add files via upload

---
+++ Pet/класс pet 1 задание.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Pet {
+public:
+    string name;
+    string animal_type;
+    int age;
+    void set_name(string n) { name = n; };
+    void set_animal_type(string t) { animal_type = t; };
+    void set_age(int a) { age = a; };
+
+    string get_name() { return name; };
+    string get_animal_type() { return animal_type; };
+    int get_age() { return age; };
+
+
+    Pet() {
+        name = "";
+        animal_type = "";
+        age = 0;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    Pet P1;
+    P1.set_name("Nastya");
+    P1.set_animal_type("Lama");
+    P1.set_age(18);
+    cout << "Name: " << P1.get_name() << endl;
+    cout << "Animal type: " << P1.get_animal_type() << endl;
+    cout << "Age: " << P1.get_age() << endl;
+
+    return 0;
+}
