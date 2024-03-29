7f43545803d8f0d252fea41e174c638cb7491f4b
trooway <160148493+trooway@users.noreply.github.com>
2024-03-29 17:03:07 +0300

    Add files via upload

---
+++ Pet/класс retailitem 5 задание.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Retailitem {
+public:
+    string description;
+    int count;
+    float price;
+    void set_description(string d) { description = d; };
+    void set_count(int c) { count = c; };
+    void set_price(float p) { price = p; };
+    string get_description() { return description; };
+    int get_count() { return count; };
+    float get_price() { return price; };
+};
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    Retailitem a[3];
+    a[0].set_description("Платье");
+    a[0].set_count(15);
+    a[0].set_price(67.45);
+
+    a[1].set_description("Лонгслив");
+    a[1].set_count(20);
+    a[1].set_price(35.30);
+
+    a[2].set_description("Кепка");
+    a[2].set_count(40);
+    a[2].set_price(15.55);
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Товар: " << a[i].get_description() << endl;
+        cout << "Количество: " << a[i].get_count() << endl;
+        cout << "Цена: " << a[i].get_price() << endl << endl;
+    }
+
+    return 0;
+}
